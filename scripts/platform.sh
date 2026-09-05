ac709a2a8d56d38a343725aedd47823571cae50b
mmundog <282106427+mmundog@users.noreply.github.com>
2026-09-05 14:31:11 -0600

    feat: detect Linux distribution

--- scripts/platform.sh
+++ scripts/platform.sh
@@ -3,13 +3,31 @@
 case "$(uname -s)" in
   Darwin)
     export DOTFILES_OS="macos"
+    export DOTFILES_DISTRO="macos"
     ;;
   Linux)
     export DOTFILES_OS="linux"
+
+    if [[ -f /etc/os-release ]]; then
+      source /etc/os-release
+
+      case "$ID" in
+        ubuntu|debian)
+          export DOTFILES_DISTRO="$ID"
+          ;;
+        fedora)
+          export DOTFILES_DISTRO="fedora"
+          ;;
+        *)
+          export DOTFILES_DISTRO="unknown"
+          ;;
+      esac
+    else
+      export DOTFILES_DISTRO="unknown"
+    fi
     ;;
   *)
-    echo "❌ Sistema operativo no soportado: $(uname -s)"
+    echo "Unsupported operating system: $(uname -s)"
     return 1
     ;;
 esac
-
